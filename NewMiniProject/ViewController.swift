5222ebf0a6e89eb79e00b8b52f934e007aad7fe1
Angela Huynh <angelathuynh5@gmail.com>
2020-06-04 15:03:29 -0400

    Initial Commit

---
+++ NewMiniProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewMiniProject
+//
+//  Created by Angela Huynh on 6/4/20.
+//  Copyright © 2020 Angela Huynh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NewMiniProjectTests/NewMiniProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  NewMiniProjectTests.swift
+//  NewMiniProjectTests
+//
+//  Created by Angela Huynh on 6/4/20.
+//  Copyright © 2020 Angela Huynh. All rights reserved.
+//
+
+import XCTest
+@testable import NewMiniProject
+
+class NewMiniProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
